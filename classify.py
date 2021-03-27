db39a691e36ccb28bdcb9cef720c1aa3249770c6
Jack Maskell <jack_maskell@hotmail.co.uk>
2021-03-27 11:30:15 +0000

    Data prep complete

--- classify.py
+++ classify.py
@@ -1,27 +1,91 @@
 from load_data import *
-SGML_PATH = "DATA/release3_2_PARSED/data/nucle3.2.sgml"
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.naive_bayes import MultinomialNB
 
-paragraphs = find_paragraphs(SGML_PATH)
+SGML_PATH = "DATA/release3_2_PARSED/data/conll14st-preprocessed.conll.ann"
+DATA_DIR = "DATA/release3_2_PARSED/data/conll14st-preprocessed.conll"
+cols = ["NID", "PID", "SID", "TOKENID", "TOKEN", "POS", "DPHEAD", "DPREL", "SYNT", "CORRECTION"]
+
+df = pd.read_table(DATA_DIR, encoding="ISO-8859-1", names=cols)
+df["CORRECTION"] = "*correct*"
+
+#paragraphs = find_paragraphs(SGML_PATH)
 mistakes = find_mistakes(SGML_PATH)
 prep_errors = filter_by_mistake_type(mistakes, "Prep")
-other_errors = filter_except_type(mistakes, "Prep")
+
+# labels = []
+# adj_words = get_adjacent_words(prep_errors[2], df)
+# incorrect_string = get_error_string(prep_errors[2], df)
+# error_loc = get_error_position(prep_errors[2], df)
+# sentence = get_sentence(prep_errors[2], df)
+# correction = correction(prep_errors[2])
+# preps["CORRECTION"] = 1
 
 
-error_strings = []
-corrected_strings = []
+for error in prep_errors:
+    mask = (df.NID == eval(error["nid"]))
+    mask &= (df.PID == eval(error["pid"]))
+    mask &= (df.SID == eval(error["sid"]))
+    mask &= (df.TOKENID >= eval(error["start_token"]))
+    mask &= (df.TOKENID <= eval(error["start_token"]))
+    df.loc[mask, "CORRECTION"] = correction(error)
+
+with open("text.txt", "w", encoding="utf-8") as f:
+    #preps = df[(df["DPREL"] == "prep")]
+    words = df["TOKEN"].to_list()
+    x = []
+    labels = df["CORRECTION"].to_list()
+
+    i = 0
+    while i < len(labels)-1:
+        adj = str(words[i-1]) + " " + str(words[i]) + " " + str(words[i+1])
+        x.append(adj)
+        #print(str(i) + "/" + str(len(labels)))
+        if labels[i] != "*correct*":
+            f.write(str(words[i]) + ">" + str(labels[i]) + " ")
+        else:
+            f.write(str(words[i]) + " ")
+        i += 1
+
+    labels = labels[:-1]
+    print(x)
+
+    X_train, X_test, y_train, y_test = train_test_split(x, labels, test_size=0.2)
+    count_vect = CountVectorizer()
+    X_train_counts = count_vect.fit_transform(X_train)
+    tfidf_transformer = TfidfTransformer()
+    X_train_tfidf = tfidf_transformer.fit_transform(X_train_counts)
+
+    clf = MultinomialNB().fit(X_train_tfidf, y_train)
+    print(clf.predict("need of habitable"))
 
 
-print("correcting mistakes")
-
-with open("test.txt", "w") as f:
-    for error in prep_errors:
-        corrected = corr_paragraph(paragraphs, error)
-        corrected_strings.append(corrected)
-        f.write(corrected)
 
 
-print(len(paragraphs))
-print(len(corrected_strings))
 
 
+
+
+    # paragraph = document[(document["PID"] == eval(error["pid"]))]
+    # sentence = paragraph[(paragraph["SID"] == eval(error["sid"]))]
+    # errors = sentence[(sentence["TOKENID"] >= eval(error["start_token"])) & (
+    #         sentence["TOKENID"] <= eval(error["end_token"]))]
+    # if not errors.empty:
+    #     df.loc[(df["DPREL"] == "prep" & df["NID"] == eval(error["nid"])), "CORRECTION"] = correction(error)
+
+
+#sentence[error_loc[0]:error_loc[len(error_loc
+#print("correcting mistakes")
+
+#with open("test.txt", "w") as f:
+    #for error in prep_errors:
+
+        #labels.append(correction(error))
+       # print(get_adjacent_words(df, error))
+
+# x = find_corrections(, prep_errors)
+# print(x)
 
--- load_data.py
+++ load_data.py
@@ -4,7 +4,7 @@ from bs4 import BeautifulSoup
 DATA_DIR = "DATA/release3_2_PARSED/data/conll14st-preprocessed.conll"
 cols = ["NID", "PID", "SID", "TOKENID", "TOKEN", "POS", "DPHEAD", "DPREL", "SYNT"]
 df = pd.read_table(DATA_DIR, encoding="ISO-8859-1", names=cols)
-SGML_DIR = "DATA/release3_2_PARSED/data/nucle3.2.sgml"
+SGML_DIR = "DATA/release3_2_PARSED/data/conll14st-preprocessed.conll.ann"
 
 
 def find_paragraphs(sgml_path):
@@ -31,6 +31,39 @@ def end_par(mistake):
     return eval(mistake["end_par"]) - 1
 
 
+def get_start_token(mistake):
+    return eval(mistake["start_token"])
+
+
+def get_end_token(mistake):
+    return eval(mistake["end_token"])
+
+
+def get_errors(mistake, filtered_table):
+    document = filtered_table[(filtered_table["NID"] == eval(mistake["nid"]))]
+    paragraph = document[(document["PID"] == eval(mistake["pid"]))]
+    sentence = paragraph[(paragraph["SID"] == eval(mistake["sid"]))]
+    errors = sentence[(sentence["TOKENID"] >= eval(mistake["start_token"])) & (sentence["TOKENID"] <= eval(mistake["end_token"]))]
+    return errors
+
+
+def get_error_string(mistake, filtered_table):
+    error = get_errors(mistake, filtered_table)
+    return error["TOKEN"].tolist()
+
+
+def get_error_position(mistake, filtered_table):
+    error = get_errors(mistake, filtered_table)
+    return error["TOKENID"].tolist()
+
+
+def get_sentence(mistake, filtered_table):
+    document = filtered_table[(filtered_table["NID"] == eval(mistake["nid"]))]
+    paragraph = document[(document["PID"] == eval(mistake["pid"]))]
+    sentence = paragraph[(paragraph["SID"] == eval(mistake["sid"]))]
+    return sentence["TOKEN"].tolist()
+
+
 def start_mistake(mistake):
     return eval(mistake["start_off"])
 
@@ -47,6 +80,14 @@ def correction(mistake):
     return mistake.find_next("correction").text
 
 
+def get_start_token(mistake):
+    return mistake["start_token"]
+
+
+def get_end_token(mistake):
+    return mistake["end_token"]
+
+
 def inc_str(paragraph, mistake):
     return paragraph[start_par(mistake)].text[start_mistake(mistake):end_mistake(mistake)]
 
@@ -57,7 +98,7 @@ def inc_paragraph(paragraph, mistake):
 
 def corr_paragraph(paragraph, mistake):
     inc_para = inc_paragraph(paragraph, mistake)
-    corrected = inc_para[:start_mistake(mistake)+1] + correction(
+    corrected = inc_para[:start_mistake(mistake) + 1] + correction(
         mistake) + inc_para[end_mistake(mistake):]
     return corrected
 
@@ -79,22 +120,45 @@ def filter_except_type(mistakes, error_type):
             filtered_mistakes.append(mistakes)
     return filtered_mistakes
 
-def get_adjacent_words():
-    print("")
-    # i = 0
-    # for mistake in mistakes:
-    #     print(str(i) + "/" + str(len(mistakes)) + " filtering error")
-    #     i += 1
-    #     if err_type(mistake) == error_type:
-    #         print("matching type found")
-    #         filtered.append(mistake)
 
-    # return filtered
-print(df["NID"])
+def get_adjacent_words(mistake, data_field):
+    document = data_field[(data_field["NID"] == eval(mistake["nid"]))]
+    paragraph = document[(document["PID"] == eval(mistake["pid"]))]
+    sentence = paragraph[(paragraph["SID"] == eval(mistake["sid"]))]
+    prev_word = sentence[(sentence["TOKENID"]) == eval(mistake["start_token"]) - 1]
+    next_word = sentence[(sentence["TOKENID"]) == eval(mistake["end_token"]) + 1]
+    error_words = get_error_string(mistake, data_field)
+    all_words = [prev_word["TOKEN"].to_string(index=False)] + error_words + [next_word["TOKEN"].to_string(index=False)]
+    return all_words
 
-# with open("DATA/release3_2_PARSED/data/nucle3.2.sgml", "r") as file:
-#     soup = BeautifulSoup(file, "html.parser")
-#     types = soup.find_all("type", text="Prep")
-#     #filtered_list = types.findPreviousSiblings
-#     print(types)
 
+def get_table_token(data_field):
+    return data_field["TOKEN"]
+
+
+def filter_table_by_pos(data_field, pos_tag):
+    filtered = data_field[(data_field["POS"] == pos_tag)]
+    return filtered
+
+
+def filter_table_by_dprel(data_field, dprel):
+    return data_field[(data_field["DPREL"] == dprel)]
+
+
+def get_parent_pos(data_field):
+    parent_pos = data_field["DPHEAD"]
+    return parent_pos
+
+
+def get_dependency_relationship(data_field):
+    dependency_relationship = data_field["DPREL"]
+    return dependency_relationship
+
+# print(table_by_pos(df, "PRP"))
+# print(df["NID"])
+# row = df.loc[3]
+# print(row["TOKEN"])
+# mistakes = find_mistakes(SGML_DIR)
+# x = get_adjacent_words(mistakes[0], df)
+# print(mistakes[0])
+# print(get_adjacent_words(mistakes[0], df))
